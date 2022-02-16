967e0b34632396d71e57f9e6b23a11f7c50114d7
zhong.jun.jimmy <zhong.jun.jimmy@JimmydeMacBook-Air.local>
2022-02-16 17:55:40 +0800

    [TASK] optimizate the letter display over one times

--- wordle-tool.py
+++ wordle-tool.py
@@ -59,18 +59,18 @@ def input_status():
             print('The length of answer MUST equal 5 and MUST be an digital.')
 
 # show answer and status following wordle rules with user entering
-def show_answer_status(str_answer, str_status):
+def show_answer_status(data_answer_array):
     count = 0
     current_status = ""
-    for status in str_status:
-        if int(status) == 0 :
-            current_status += bcolors.UNEXIST + str_answer[count] + bcolors.ENDC
+    for data in data_answer_array:
+        if (data[1] == 0) :
+            current_status += bcolors.UNEXIST + data[0] + bcolors.ENDC
             count+=1
-        elif int(status) == 1 :
-            current_status += bcolors.CURRECT + str_answer[count] + bcolors.ENDC
+        elif (data[1] == 1) :
+            current_status += bcolors.CURRECT + data[0] + bcolors.ENDC
             count+=1
-        elif int(status) == 2 :
-            current_status += bcolors.WrongPos + str_answer[count] + bcolors.ENDC
+        elif (data[1] == 2) :
+            current_status += bcolors.WrongPos + data[0] + bcolors.ENDC
             count+=1
     print(current_status)
 
@@ -93,11 +93,62 @@ def sorted_by_wrong_pos_letter(wordlist, letterArray):
 
 
 # compare and match by regex, return recommand list
-def check_recommand(str_answer, str_status, wordlist):
+def check_recommand(str_answer, str_status, data_answer_array, wordlist):
+    letterArray = []
+    str_match_correct_letter_reg = ''
+    for i in range(0, 5):
+        if data_answer_array[i][1] == 0:
+            status = [0,0,0,0,0]
+            for j in range(0, 5):
+                # print(data_answer_array[j][0])
+                # print(data_answer_array[i][0])
+                if data_answer_array[j][0] == data_answer_array[i][0]:
+                    if data_answer_array[j][1] == 1:
+                        status[j] = 1
+                    elif data_answer_array[j][1] == 2:
+                        status[j] = 2
+            if 2 in status:
+                list_match_reg = list('^.....$')
+                list_match_reg[i] = '[^'+data_answer_array[i][0]+']'
+                str_match_reg = ''.join(list_match_reg)
+                # print(str_match_reg)
+                wordlist = [w for w in wordlist if re.match(str_match_reg, w.lower())]
+            else:
+                list_match_reg = list('^.....$')
+                for j in range(0, 5):
+                    if status[j] == 0:
+                        list_match_reg[j+1] = '[^' + data_answer_array[i][0] + ']'
+                    elif status[j] == 1:
+                        list_match_reg[j+1] = data_answer_array[i][0]
+                str_match_reg = ''.join(list_match_reg)
+                # print(str_match_reg)
+                wordlist = [w for w in wordlist if re.match(str_match_reg, w.lower())]
+
+        elif data_answer_array[i][1] == 1:
+            list_match_correct_letter_reg = list('^.....$')
+            list_match_correct_letter_reg[i+1] = data_answer_array[i][0]
+            str_match_correct_letter_reg = ''.join(list_match_correct_letter_reg)
+            # print(str_match_correct_letter_reg)
+            wordlist = [w for w in wordlist if re.match(str_match_correct_letter_reg, w.lower())]
+            
+        
+        elif data_answer_array[i][1] == 2:
+            letterArray.append(data_answer_array[i][0])
+            list_match_reg = list('^.....$')
+            list_match_reg[i+1] = '[^' + data_answer_array[i][0] + ']'
+            str_match_reg = ''.join(list_match_reg)
+            # print(str_match_reg)
+            wordlist = [w for w in wordlist if re.match(str_match_reg, w.lower())]
+
+
+        if(len(letterArray) != 0):
+            wordlist = sorted_by_wrong_pos_letter(wordlist, letterArray)
+                  
+    # print(wordlist)
+
+    '''
     letterArray = []
     list_regex_current = list('^.....$')
-
-    
     count = 0
     current_status = ""
     for status in str_status:
@@ -134,7 +185,7 @@ def check_recommand(str_answer, str_status, wordlist):
         # print(wordlist[:20])
     #print(wordlist)
     #sort_by_rule(str_answer, str_status, wordlist)
-    #print(wordlist)
+    #print(wordlist)'''
     return wordlist # return top 20 result
 
 # show recommand list by table
@@ -153,10 +204,16 @@ def main():
     # print([w for w in wordlist if re.search('^..a..', w)])
 
     while True:
+        data_answer_array = []
+
         str_answer = input_answer()
         str_status = input_status()
-        show_answer_status(str_answer, str_status)
-        wordlist = check_recommand(str_answer, str_status, wordlist)
+
+        for i in range(0,5):
+            data_answer_array.append([str_answer[i], int(str_status[i])])
+
+        show_answer_status(data_answer_array)
+        wordlist = check_recommand(str_answer, str_status, data_answer_array, wordlist)
         show_recommand_word(wordlist)
 
 # run main function
